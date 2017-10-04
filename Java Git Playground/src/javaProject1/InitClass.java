29c48236e1c311acd6fdff4da5b77e5309033820
geekkid1 <ibtronfan@gmail.com>
2017-10-04 12:56:04 -0400

    Some quick housekeeping changes to prepare for merge

--- Java Git Playground/src/javaProject1/InitClass.java
+++ Java Git Playground/src/javaProject1/InitClass.java
@@ -1,15 +1,6 @@
 package javaProject1;
-/**
- * 
- * 
- * @author Isaac Blum
- */
 public class InitClass {
-	/**
-	 * At this point, the main function does pretty much nothing. contains a comment. that's it
-	 * @param args : don't pass anything through here. it won't do anything with it
-	 */
 	public static void main(String[] args) {
-		//Check out the beta extension branch for lots of mentions of the word cat.
+		
 	}
 }
