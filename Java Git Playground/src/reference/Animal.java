63cc55beededc6527f8be82ac55e3be8ac759c1c
geekkid1 <ibtronfan@gmail.com>
2017-10-04 10:09:46 -0400

    Create beta extensions branch for second attempt at extensions and package imports practice.

---
+++ Java Git Playground/src/reference/Animal.java
@@ -0,0 +1,11 @@
+package reference;
+/** 
+ * Parent class for all Animal type objects.
+ * <p>
+ * Check here for any methods you want to use and descriptions of them
+ * @author Isaac Blum
+ */
+public class Animal {
+
+
+}
