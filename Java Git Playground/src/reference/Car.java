c12a21b6aca752f35f4be64f03eb9892abf7179b
geekkid1 <ibtronfan@gmail.com>
2017-10-04 09:11:31 -0400

    Add more fields and a method
    Add more info fields to the superclass Car, plus a method that gets the car's mileage.

--- Java Git Playground/src/reference/Car.java
+++ Java Git Playground/src/reference/Car.java
@@ -4,4 +4,17 @@ public class Car {
 	int wheels = 4;
 	String make;
 	String model;
+	int doors;
+	double mpg;
+	int fuelCap;
+	double mileage;
+	double fuel; {
+		if (fuel > fuelCap)	 {
+			fuel = fuelCap;
+		}
+}
+	public double getMileage() {
+		this.mileage = mpg * fuel;
+		return this.mileage;
+	}
 }
