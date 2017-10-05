380b70ef7f903ff4ffe40db86a46691c2078cb76
geekkid1 <ibtronfan@gmail.com>
2017-10-05 08:01:30 -0400

    Addition of lines of code that should have been in the last commit

--- Java Git Playground/src/scannerPractice/MainClass.java
+++ Java Git Playground/src/scannerPractice/MainClass.java
@@ -1,10 +1,8 @@
 package scannerPractice;
-
+import java.util.Scanner;
 public class MainClass {
-
 	public static void main(String[] args) {
-		
+		Scanner input = new Scanner(System.in);
 
 	}
-
 }
